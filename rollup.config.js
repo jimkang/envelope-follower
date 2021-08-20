806608fea0f8d7f3769451394fcab6a0cf334233
Jim Kang <jimkang@gmail.com>
2021-08-19 21:03:12 -0400

    Fix demo build.

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@
 import createConfig from './rollup-tools/base-config';
 import { serve } from './rollup-tools/config-tools';
 
-var channelVcdrs = ['liner'];
+var channelVcdrs = [];
 
 // Inspired by https://github.com/Tom-Siegel/multi-page-svelte/blob/5dd47f9ffe3cbddbaa5e29be5056ce1ed56060b2/rollup-pages.config.js#L45
 var configs = [
@@ -21,7 +21,11 @@ var configs = [
       outputFile: `envelope-followers/${v}/${v}-envelope-follower-bundle.js`,
       reloadPath: `envelope-followers/${v}`,
       serve: process.env.APP === v && serve,
-      serveOpts: { rootDir: '.', serveDir: `envelope-followers/${v}`, port: 6001 },
+      serveOpts: {
+        rootDir: '.',
+        serveDir: `envelope-followers/${v}`,
+        port: 6001,
+      },
     }))
   )
   .map(createConfig);
